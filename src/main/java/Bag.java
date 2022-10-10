66a59034f29410923007f0c8d3b70eefd53b9333
cherylzyzhang <102629472+cherylzyzhang@users.noreply.github.com>
2022-10-09 23:20:59 -0400

    done

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -5,79 +5,75 @@
  * 1. Introduction to Java helpful.
  */
 
+import java.sql.Struct;
+
 public abstract class Bag {
-    /*
-     * TODO: Create the following private instance variables
-     *       - a String named color
-     *       - an int named numberOfContents
-     *       - an int named capacity
-     *       - an array of Strings named contents
-     */
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
 
+    public Bag(String color, int capacity){
+        this.color = color;
+        this.capacity = capacity;
+        this.numberOfContents = 0;
+        this.contents = new String[] {};
+    }
 
 
-    /*
-     * TODO: Create a constructor that takes two arguments:
-     *       - a String representing the Bag's colour
-     *       - an int representing the Bag's capacity
-     *
-     * The other attributes (private instance variables) should
-     * be empty (e.g. numberOfContents is 0 and an empty String array for
-     * its contents.)
-     */
+    public String getColor(){
+        return this.color;
+    }
 
 
+    public int getCapacity(){
+        return this.capacity;
+    }
 
 
-    /*
-     * TODO: Create a variety of 'getter' functions.
-     *       These should be named:
-     *           - getColor
-     *           - getNumberOfContents
-     *           - getCapacity
-     */
+    public int getNumberOfContents(){
+        return this.numberOfContents;
+    }
 
 
+    public void setColor(String color){
+        this.color = color;
+    }
 
 
-    /*
-     * TODO: Create a setter function called setColor which sets the
-     *       color of this bag to the given color.
-     */
-
-
-
-
-
-    /*
-     * TODO: Create a method called addItem that takes in a String
-     *       representing an item in the Bag.
-     *       The item is added into the Bag if the number of items
-     *       in the bag is < the capacity of the Bag.
-     *       Remember to modify numberOfContents accordingly.
-     *
-     *       This method should return true if the item was added
-     *       and false otherwise.
-     */
+    public boolean addItem(String item){
+        if (numberOfContents < capacity){
+            String[] new_contents = new String[numberOfContents + 1];
+            System.arraycopy(contents, 0, new_contents, 0, numberOfContents);
+            new_contents[numberOfContents] = item;
+            contents = new_contents;
+            numberOfContents ++;
+            return true;
+        }
+        else {return false;}
+    }
 
 
 
 
 
     /**
-     * TODO: Create a method called popItem that returns a String.
-     *       The string should be the last item added to this Bag
-     *       and the item should be removed from this Bag.
-     *       Remember to modify numberOfContents accordingly.
-     *
      * If there are no items in this Bag, return null.
      *
      * @return
      */
-
-
-
+    public String popItem(){
+        if (numberOfContents != 0){
+            String last_item = new String(contents[numberOfContents - 1]);
+            String[] new_contents = new String[numberOfContents - 1];
+            System.arraycopy(contents, 0, new_contents, 0, numberOfContents - 1);
+            contents = new_contents;
+            numberOfContents --;
+            return last_item;
+        }
+        else {return null;}
+    }
 
 
     /**
@@ -86,9 +82,9 @@ public abstract class Bag {
      * @param n the amount to increase this Bag's capacity by
      */
     public void increaseCapacity(int n) {
-        // TODO: Implement this method.
-
+        capacity += n;
     }
+
 
     /**
      * Return the details of this Bag.
--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -5,21 +5,41 @@
  * You may find the readings in chapter 3. Relationships between Classes
  * helpful while working through this exercise.
  */
+public class CrossbodyBag extends Bag{
+    /**
+     * Creates a new CrossbodyBag with the given color,
+     * capacity, and number of straps.
+     *
+     * @param color
+     * @param capacity
+     * @param numberOfStraps
+     */
+    private int numberOfStraps;
 
+    public CrossbodyBag(String color, int capacity, int numberOfStraps) {
+        super(color, capacity);
+        this.numberOfStraps = numberOfStraps;
+    }
+
+
+    public int getNumberOfStraps(){
+        return numberOfStraps;
+    }
+
+
+    @Override
+    public void enhance() {
+        super.increaseCapacity(2);
+    }
+
+
+    @Override
+    public String toString() {
+        return this.getColor() + " Crossbody Bag with " + this.getNumberOfStraps() + " straps (" +
+                this.getNumberOfContents() + " / " + this.getCapacity() + ")";
+    }
+}
 /*
- * TODO: Create a public class named CrossbodyBag which is a subclass of Bag
- *       In addition to the attributes in Bag, the CrossbodyBag should have an
- *       attribute named "numberOfStraps".
- *
- *       The constructor for CrossbodyBag should take the following parameters
- *       in order:
- *           1. a String for the color
- *           2. an int for the capacity
- *           3. an int for the number of straps
- *
- *       CrossbodyBag should also have a getter method called getNumberOfStraps
- *       and it should implement the enhance method.
- *
  *       The enhance method should increase the capacity of the CrossbodyBag by
  *       2 (as opposed to the 1 that HandBag increases by).
  *
